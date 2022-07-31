efa9cdcbb9d4bcf9319a21e56c4c7334c1e4aee8
Bipul Chaudhary <bipulchau@gmail.com>
2022-07-31 13:49:28 -0400

    Get users details from the database

--- dao/users_dao.py
+++ dao/users_dao.py
@@ -1,5 +1,20 @@
+from decouple import config
+# https://able.bio/rhett/how-to-set-and-get-environment-variables-in-python--274rgt5#:~:text=To%20set%20and%20get%20environment%20variables%20in%20Python%20you%20can,Get%20environment%20variables%20USER%20%3D%20os.
+import psycopg
+
+API_HOST = config('host')
+API_PORT = config('port')
+API_DBNAME = config('dbname')
+API_USER = config('user')
+API_PASSWORD = config('password')
+
+
 class UsersDao:
     @staticmethod
     def get_users():
-        return "Get users at DAO"
-
+        with psycopg.connect(host=API_HOST, port=API_PORT, dbname=API_DBNAME, user=API_USER,
+                             password=API_PASSWORD) as conn:
+            with conn.cursor() as cur:
+                cur.execute("select * from climawatch.users")
+                user_info = cur.fetchall()
+                return {"users": user_info}
