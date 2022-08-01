0adf46ad21425129085b0807e9e54fbf32c194b4
Bipul Chaudhary <bipulchau@gmail.com>
2022-08-01 18:54:41 -0400

    Can get,post and delete only when the user is logged in

--- controller/locations_controller.py
+++ controller/locations_controller.py
@@ -12,19 +12,25 @@ def get_location(username):
         if session["user_info"]["username"] == username:
             if request.method == "GET":
                 return LocationService.get_location(username)
+            #     if session.get("user_info"):
+            #         if session["user_info"]["username"] == username:
+            #             return LocationService.get_location(username)
 
-        elif request.method == "POST":  # ***************** POST - add new location
-            try:
-                data = request.get_json()
-                return LocationService.add_location(data, username)
-            except LocationAlreadyExistError as e:
-                return {"message": str(e)}
-        else:  # ***************** DELETE - delete existing location
-            try:
-                data = request.get_json()
-                return LocationService.delete_location(data, username)
-            except LocationNotFoundError as e:
-                return {"message": str(e)}, 404
+            elif request.method == "POST":  # ***************** POST - add new location
+                try:
+                    data = request.get_json()
+                    return LocationService.add_location(data, username)
+                except LocationAlreadyExistError as e:
+                    return {"message": str(e)}
+
+            else:  # ***************** DELETE - delete existing location
+                try:
+                    data = request.get_json()
+                    return LocationService.delete_location(data, username)
+                except LocationNotFoundError as e:
+                    return {"message": str(e)}, 404
     else:
         return "Please log in first!!", 401
+
+
 
