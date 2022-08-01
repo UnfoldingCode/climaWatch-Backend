3f19e61723318750e7054fd4f7439de0eb95d2b5
Bipul Chaudhary <bipulchau@gmail.com>
2022-07-31 20:35:55 -0400

    get location details of a user

--- dao/locations_dao.py
+++ dao/locations_dao.py
@@ -1,4 +1,20 @@
+from decouple import config
+# https://able.bio/rhett/how-to-set-and-get-environment-variables-in-python--274rgt5#:~:text=To%20set%20and%20get%20environment%20variables%20in%20Python%20you%20can,Get%20environment%20variables%20USER%20%3D%20os.
+import psycopg
+
+API_HOST = config('host')
+API_PORT = config('port')
+API_DBNAME = config('dbname')
+API_USER = config('user')
+API_PASSWORD = config('password')
+
+
 class LocationDao:
     @staticmethod
     def get_location(username):
-        return f"Location at Dao and username is {username}"
+        with psycopg.connect(host=API_HOST, port=API_PORT, dbname=API_DBNAME, user=API_USER,
+                             password=API_PASSWORD) as conn:
+            with conn.cursor() as cur:
+                cur.execute("select * from climawatch.locations where users_username=%s", (username,))
+                locations_info = cur.fetchall()
+                return {"locations": locations_info}
