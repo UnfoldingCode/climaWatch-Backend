cf2edf7494799f45a7c01cb786f29960aa3452e9
Bipul Chaudhary <bipulchau@gmail.com>
2022-08-01 16:57:06 -0400

    Can get,post and delete only when the user is logged in

--- controller/locations_controller.py
+++ controller/locations_controller.py
@@ -8,21 +8,23 @@ lc = Blueprint("location_controller", __name__)
 
 @lc.route("/locations/<username>", methods=["GET", "POST", "DELETE"])
 def get_location(username):
-    if request.method == "GET":
-        if session.get("user_info"):
-            if session["user_info"]["username"] == username:
+    if session.get("user_info"):
+        if session["user_info"]["username"] == username:
+            if request.method == "GET":
                 return LocationService.get_location(username)
-        else:
-            return "Please log in first!!", 401
-    elif request.method == "POST":  # ***************** POST - add new location
-        try:
-            data = request.get_json()
-            return LocationService.add_location(data, username)
-        except LocationAlreadyExistError as e:
-            return {"message": str(e)}
-    else:  # ***************** DELETE - delete existing location
-        try:
-            data = request.get_json()
-            return LocationService.delete_location(data, username)
-        except LocationNotFoundError as e:
-            return{"message": str(e)}, 404
+
+        elif request.method == "POST":  # ***************** POST - add new location
+            try:
+                data = request.get_json()
+                return LocationService.add_location(data, username)
+            except LocationAlreadyExistError as e:
+                return {"message": str(e)}
+        else:  # ***************** DELETE - delete existing location
+            try:
+                data = request.get_json()
+                return LocationService.delete_location(data, username)
+            except LocationNotFoundError as e:
+                return {"message": str(e)}, 404
+    else:
+        return "Please log in first!!", 401
+
